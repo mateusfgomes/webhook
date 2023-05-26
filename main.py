acae8d9b0408b052f163ace50fa842eb0f27df15
Mateus Ferreira <39449176+mateusfgomes@users.noreply.github.com>
2023-05-26 16:23:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ from flask import request
 
 app = Flask(__name__)
 
+@app.route("/webhooks", methods=["GET, POST"])
+def webhooks():
+	return f"data:{request.args.get("data")}"
+
+@app.route("/", methods=["GET, POST"])
+def start():
+	return f"{request.args.get("data")}"
+
 @app.route("/facebook", methods=["GET, POST"])
-def webhook():
-	return f"data:{request.args.get("string")}"
-	
+def facebook():
+	return f"{request.args.get("data")}"
+
 if __name__ == "__main__":
 	app.run()
